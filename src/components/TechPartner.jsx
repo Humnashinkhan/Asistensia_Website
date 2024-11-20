d36863e23d825aa90b88eae8b89b8eafacb4b72a
Humnashin Khan <humnashin9955@gmail.com>
2024-11-20 09:54:52 +0530

    Techpartner components

--- src/components/TechPartner.jsx
+++ src/components/TechPartner.jsx
@@ -2,7 +2,56 @@ import React from 'react'
 
 const TechPartner = () => {
   return (
-    <div>TechPartner</div>
+    <div className='w-full md:h-[140vh]'>
+      <div className="md:ml-44 md:mt-24">
+        <div className="md:flex">
+          <img 
+          className='h-[26rem]'
+          alt="img1"
+          src="https://asistensia.com/assets/features-1-73eac8aadfe7604f48729ddb3a7d668d20a9db7a8818cb630fee4c45a5734d5f.svg"
+        />
+        <div className="ml-8">
+          <h4 className="text-[24px] text-[#444] font-semibold">Asistensia is your technology partner in<br/>
+             product development.</h4>
+          <p className="text-[16px] pt-5 text-[#444] font-normal">Our workflow is divided into three core processes: ideation and evaluation,<br/> 
+            product design, and product development. Quality assurance is an integral<br/> 
+            part of both of these processes.</p>
+          <p className="text-[16px] pt-5 text-[#444] font-normal">Dealing with a new product, at this stage, we want to understand how<br/> 
+            promising a business plan is, will a product be viable and what markets can<br/> 
+            be interested in it.</p>
+        </div>
+        </div>
+        </div>
+        <div className="md:flex md:ml-44 mt-24 p-3">
+        <div>
+        <h4 className="text-[24px] text-[#444] font-semibold">Infrastructure management and DevOps.</h4>
+            <p className="text-[16px] pt-5 text-[#444] font-normal">Management and support are the ongoing processes that are present<br/>
+              across the whole life cycle of a project.</p>
+
+            <p className="text-[16px] pt-7 text-[#444] font-normal">We maintain the integrity of our code and provide regular updates. Our<br/>  
+              ongoing maintenance packages include software updates, website<br/>  
+              speed optimization, backups, compliance with constantly updated web standards,<br/>  
+              user experience improvements, security improvements, and more..
+            </p>
+            <p className="text-[16px] pt-7 text-[#444] font-normal">Business-critical enterprise solutions normally take lots of time to develop.<br/>  
+              With our efficient engineering processes, we implement enterprise features<br/>  
+              fast while ensuring scalability and flexibility. We can help you transform your<br/> 
+
+              organization with custom software that runs quickly and smoothly.
+            </p>
+            </div>
+          <div className="md:ml-8">
+          <img 
+          className='h-[26rem]'
+          alt="img2"
+          src="https://asistensia.com/assets/features-2-6d40356f6f4776c0dc8746464c0491d5605bfd6aed7033382be0d960c355d991.svg"
+          />
+        </div>
+        </div>
+</div>
+
+      
+
   )
 }
 
